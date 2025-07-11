74bb1f6f101e8e4a43cad758040094d9bd0486fa
arv20 <aarav.kolgaonkar@gmail.com>
2025-07-10 20:37:28 -0400

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Aarav Kolgaonkar on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
